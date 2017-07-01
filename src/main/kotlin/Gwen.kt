2d7c367c08f5bfa1b7a7104684e99513d4a6bd22
badlogic <contact@badlogicgames.com>
2017-07-01 22:00:21 +0200

    OS detection for shared lib loading

--- src/main/kotlin/Gwen.kt
+++ src/main/kotlin/Gwen.kt
@@ -4,7 +4,16 @@ package com.badlogicgames.gwen
 import ai.kitt.snowboy.SnowboyDetect
 
 fun main(args: Array<String>) {
-    System.loadLibrary("snowboy-detect-java");
+    val osName = System.getProperty("os.name").toLowerCase();
+    val osArch = System.getProperty("os.arch").toLowerCase();
+
+    println("OS name: ${osName}");
+    println("OS arch: ${osArch}");
+
+    when {
+        osName.contains("mac") -> System.load("jni/libsnowboy-detect-java.dylib");
+        else -> System.load("jni/libsnowboy-detect-java.so");
+    }
 
     val detector = SnowboyDetect("models/snowboy/common.res", "models/snowboy/OK Google.pmdl");
     val recorder = AudioRecorder(16000f);
