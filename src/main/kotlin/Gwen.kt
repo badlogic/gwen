df5f9936184adc72b44936d6971486d3fbb1eebc
badlogic <contact@badlogicgames.com>
2017-07-01 21:53:48 +0200

    Basic hotword detection via snowball

--- src/main/kotlin/Gwen.kt
+++ src/main/kotlin/Gwen.kt
@@ -1,7 +1,19 @@
 @file:JvmName("Gwen")
 package com.badlogicgames.gwen
 
-fun main(args: Array<String>) {
+import ai.kitt.snowboy.SnowboyDetect
 
-    println("Hello Gwen");
+fun main(args: Array<String>) {
+    System.loadLibrary("snowboy-detect-java");
+
+    val detector = SnowboyDetect("models/snowboy/common.res", "models/snowboy/OK Google.pmdl");
+    val recorder = AudioRecorder(16000f);
+
+    while (true) {
+        val data = recorder.read();
+        val result = detector.RunDetection(data, data.size);
+        if (result > 0) {
+            println("Hotword ${result} detected!");
+        }
+    }
 }
